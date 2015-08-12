b39549fe8574bbebaf23c54f31a58b1e4f936cf2
Guido <gvilarino@gmail.com>
2015-08-12 18:01:32 -0300

    Refactor root index.js to use lib/boot

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-import express from 'express'
-import bodyParser from 'body-parser'
 import debug from 'debug'
+import app from 'lib/boot'
 
 let log = debug('api-starter:root')
-let app = express()
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// Your custom API should be initialized here
-// See examples/index.js for more information
 
 let port = 3000
 
